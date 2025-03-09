091f3af22bceb462c6092610be2f5f57ca1bba81
Zenati Mehdi <mehdi1.zenati@epitech.eu>
2025-03-09 17:23:09 +0100

    updated to a method

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import openAiRequest from './openAiAPI.ts'
 dotenv.config();
 
 const app = express();
 const port = 3265;
 
-app.get('/', (req, res) => {
-  res.send('Express + TypeScript Server');
+app.get('/', async (req, res) => {
+  res.send("test");
 });
 
+app.get('/request', async (req, res) => {
+  const response = await openAiRequest("Ecris en anglais", "https://cdn11.bigcommerce.com/s-sdhub7he5o/images/stencil/1280x1280/products/1596/5390/item_image04__67133.1673460026.jpg?c=1");
+  res.send(response);
+});
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
+
 });
--- server/openAiAPI.ts
+++ server/openAiAPI.ts
@@ -1,29 +1,37 @@
 import OpenAI from "openai";
+import type { ChatCompletionContentPartText } from "openai/resources/chat/completions/completions.mjs";
 const openai = new OpenAI({
     organization: "org-kmATHO0II7YUN5oacXUQQpvG",
     project: "proj_ENvKq4O4BQUXxphvSPFdzraZ",
 });
 
-const completion = await openai.chat.completions.create({
+export default async function openAiRequest(prompt = "" as String , img = "https://ihe-delft-ihe-website-production.s3.eu-central-1.amazonaws.com/s3fs-public/styles/530x530/public/2022-11/Sea%20level%20rise%20Vietnam.jpg" as ChatCompletionContentPartText["text"] ){
+
+  const completion = await openai.chat.completions.create({
     model: "gpt-4o-mini",
     messages:[
-        {
-          "role": "user",
-          "content": [
-            {"type": "text", "text": "Qu'est-ce qu'il y a dans cette image? et essaie de deviner le pays"},
-            {
-              "type": "image_url",
-              "image_url": {
-                "url": "https://ihe-delft-ihe-website-production.s3.eu-central-1.amazonaws.com/s3fs-public/styles/530x530/public/2022-11/Sea%20level%20rise%20Vietnam.jpg",
-              },
+      {
+        "role": "user",
+        "content": [
+          {"type": "text", "text": prompt + ". Décris ce que tu vois dans cette image?"},
+          {
+            "type": "image_url",
+            "image_url": {
+              "url": img,
             },
-          ],
-        }
-      ],
-      store: true,
-      max_tokens: 300,    
-    },
+          },
+        ],
+      }
+    ],
+    store: true,
+    max_tokens: 300,    
+  },
 );
-
 console.log(completion.choices[0].message.content);
-console.log(completion.usage)
+// console.log(completion.usage)
+return completion.choices[0].message.content;
+}
+
+// test();
+// exports.completion = test;
+// module.exports={completion}
